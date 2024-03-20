261b78fa65363ba2111208e2713ef58bc55dfcde
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2024-03-21 01:22:11 +0530

    Update watcher.py

--- StenzleMusic/Modules/watcher.py
+++ StenzleMusic/Modules/watcher.py
@@ -55,10 +55,18 @@ async def on_stream_end(pytgcalls, update: Update):
             return await pytgcalls.leave_group_call(chat_id)
         except:
             return
+    else:
+        process = await app.send_message(
+            chat_id=chat_id,
+            text="» ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ɴᴇxᴛ ᴛʀᴀᴄᴋ ғʀᴏᴍ ᴏ̨ᴜᴇᴜᴇ...",
+        )
+            await process.delete()
         title = get[0]["title"]
         duration = get[0]["duration"]
         file_path = get[0]["file_path"]
         videoid = get[0]["videoid"]
+        req_by = get[0]["req"]
+        user_id = get[0]["user_id"]
         get.pop(0)
 
         stream = AudioPiped(file_path, audio_parameters=HighQualityAudio())
@@ -71,3 +79,9 @@ async def on_stream_end(pytgcalls, update: Update):
         except:
             await _clear_(chat_id)
             return await pytgcalls.leave_group_call(chat_id)
+
+
+
+        
+        
+
