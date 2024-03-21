1e49cca8c6200814e22d9adcfc2c220917956aff
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2024-03-21 18:06:33 +0530

    Update __init__.py

--- StenzleMusic/__init__.py
+++ StenzleMusic/__init__.py
@@ -1,4 +1,5 @@
 
+
 import asyncio
 import logging
 import os
@@ -14,7 +15,7 @@ StartTime = time.time()
 logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.FileHandler("Stenzlelogs.txt"), logging.StreamHandler()],
+    handlers=[logging.FileHandler("fallenlogs.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -83,7 +84,7 @@ async def Stenzle_startup():
     elif int(ANON) not in config.SUDO_USERS:
         SUDOERS.add(int(ANON))
 
-    Stenzledb = {}
+    fallendb = {}
     LOGGER.info(
         "[•] \x4c\x6f\x63\x61\x6c\x20\x44\x61\x74\x61\x62\x61\x73\x65\x20\x49\x6e\x69\x74\x69\x61\x6c\x69\x7a\x65\x64\x2e\x2e\x2e"
     )
