850d9dedf9d818ffedcb7e5cabe2bb182a5ffe47
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2024-03-21 17:25:29 +0530

    Add files via upload

---
+++ StenzleMusic/modules/stop.py
@@ -0,0 +1,23 @@
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from StenzleMusic import app, pytgcalls
+from StenzleMusic.Helpers import _clear_, admin_check, close_key
+
+
+@app.on_message(filters.command(["stop", "end", "e"]) & filters.group)
+@admin_check
+async def stop_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        await _clear_(message.chat.id)
+        await pytgcalls.leave_group_call(message.chat.id)
+    except:
+        pass
+
+   
